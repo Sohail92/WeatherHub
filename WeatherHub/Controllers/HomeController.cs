9fd228d2d751c43d54693a956d8c617174daa1c3
Sohail Rahman <contact@sohailrahman.net>
2019-07-28 16:57:02 +0100

    Removal of unused code and files

--- WeatherHub/Controllers/HomeController.cs
+++ WeatherHub/Controllers/HomeController.cs
@@ -21,16 +21,5 @@ namespace WeatherHub.Controllers
                      
             return View("Index", sb.ToString());
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
