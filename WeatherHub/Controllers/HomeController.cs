cfb8b5e098108647ba3a883a8358e85fffcb66cf
Sohail Rahman <contact@sohailrahman.net>
2019-08-01 22:48:51 +0100

    Seperated pages for home view and for search results

--- WeatherHub/Controllers/HomeController.cs
+++ WeatherHub/Controllers/HomeController.cs
@@ -9,14 +9,12 @@ namespace WeatherHub.Controllers
     {
         public IActionResult Index()
         {
-            return View("Index", new List<SupplierInformation> { });
+            return View("Index");
         }
 
         public IActionResult Search(string location)
         {
-            // Notes:
-            // Could cache these calls in the future based on the location e.g. Keep Middlesbrough cached for 15mins.
-            // Hard coded middlesbrough for now, base weather info on users search param in a future version.
+            // Notes: could cache the calls based on the location e.g. Keep Middlesbrough cached for 15mins.
 
             // Initialise list of weather information and append from each available source.
             List<SupplierInformation> weatherInformation = new List<SupplierInformation>();
@@ -24,7 +22,7 @@ namespace WeatherHub.Controllers
             weatherInformation.Add(new OpenWeather().GetWeatherInformation(location));
             weatherInformation.Add(new AccuWeather().GetWeatherInformation(location));
 
-            return View("Index", weatherInformation);
+            return View("~/Views/SearchResults/SearchResults.cshtml", weatherInformation);
         }
     }
 }
